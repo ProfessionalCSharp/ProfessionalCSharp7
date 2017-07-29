9f84590eb05388c9cc895e348aa682e2b260d1c2
Christian Nagel <christian@christiannagel.com>
2017-07-29 22:41:25 +0200

    cleanup spans

--- Memory/MemorySamples/SpanSample/Program.cs
+++ Memory/MemorySamples/SpanSample/Program.cs
@@ -3,9 +3,11 @@ using System.Runtime.InteropServices;
 
 namespace SpanSample
 {
-    struct OuterSpan
+
+    struct CombineSpans
     {
-        private Span<int> innerSpan;
+        private Span<int> span1;
+        private Span<int> span2;
     }
 
     class Program
@@ -15,14 +17,14 @@ namespace SpanSample
             SpanOnTheHeap();
             SpanOnTheStack();
             SpanOnNativeMemory();
-            DangerousSpan();
-            SpanWithRef();
+            //DangerousSpan();
+            //SpanWithRef();
         }
 
         private static void SpanWithRef()
         {
-            OuterSpan os;
-            os.ToString();
+            CombineSpans os;
+            int hash = os.GetHashCode();
         }
 
         private static void DangerousSpan()
@@ -52,61 +54,53 @@ namespace SpanSample
         private static unsafe void SpanOnNativeMemory()
         {
             Console.WriteLine(nameof(SpanOnNativeMemory));
-            const int nbytes = 1000;
+            const int nbytes = 100;
             IntPtr p = Marshal.AllocHGlobal(nbytes);
             try
-            {
-
+            {              
                 int* p2 = (int*)p.ToPointer();
-
+                
                 Span<int> span = new Span<int>(p2, nbytes >> 2);
                 span.Fill(42);
 
-                int max = 1000 >> 2;
+                int max = nbytes >> 2;
                 for (int i = 0; i < max; i++)
                 {
-                    Console.Write(span[i]);
+                    Console.Write($"{span[i]} ");
                 }
-
             }
             finally
             {
                 Marshal.FreeHGlobal(p);
             }
             Console.WriteLine();
-
         }
 
         private static unsafe void SpanOnTheStack()
         {
             Console.WriteLine(nameof(SpanOnTheStack));
+
             long* lp = stackalloc long[20];
             var span1 = new Span<long>(lp, 20);
             
-            Span<long> slice1 = span1.Slice(0, 20);
             for (int i = 0; i < 20; i++)
             {
-                slice1[i] = i;
+                span1[i] = i;
             }
 
-            foreach (long item in span1.ToArray())
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine(string.Join(", ", span1.ToArray()));
             Console.WriteLine();
         }
 
         private static void SpanOnTheHeap()
         {
             Console.WriteLine(nameof(SpanOnTheHeap));
-            var span1 = new Span<int>(new int[] { 1, 5, 11, 71, 22, 19, 21, 33 });
+            Span<int> span1 = (new int[] { 1, 5, 11, 71, 22, 19, 21, 33 }).AsSpan();
 
-            span1.Slice(2, 4).Fill(42);
+            span1.Slice(start: 4, length: 3).Fill(42);
 
-            foreach (int i in span1.ToArray())
-            {
-                Console.WriteLine(i);
-            }
+            Console.WriteLine(string.Join(", ", span1.ToArray()));
+
             Console.WriteLine();
         }
     }

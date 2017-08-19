c3b45274f3d0eaa5ab4b52cdd7add3dcce5dbd1c
Christian Nagel <christian@christiannagel.com>
2017-08-19 23:56:37 +0200

    DI service lifetime

---
+++ DependencyInjection/DependencyInjectionSamples/ServicesLifetime/IServiceB.cs
@@ -0,0 +1,7 @@
+﻿namespace ServicesLifetime
+{
+    public interface IServiceB
+    {
+        void B();
+    }
+}
---
+++ DependencyInjection/DependencyInjectionSamples/ServicesLifetime/NumberService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+
+namespace ServicesLifetime
+{
+    public class NumberService : INumberService
+    {
+        private int _number = 0;
+        public int GetNumber() => Interlocked.Increment(ref _number);
+    }
+}

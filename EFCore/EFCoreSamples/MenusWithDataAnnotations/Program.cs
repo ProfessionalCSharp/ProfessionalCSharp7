087d9f7a0a2a4805ce8e8478e77446e37fb58e64
Christian Nagel <christian@christiannagel.com>
2017-08-26 18:08:23 +0200

    cleanup

--- EFCore/EFCoreSamples/MenusWithDataAnnotations/Program.cs
+++ EFCore/EFCoreSamples/MenusWithDataAnnotations/Program.cs
@@ -4,7 +4,7 @@ namespace MenusWithDataAnnotations
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             CreateDatabase();
         }

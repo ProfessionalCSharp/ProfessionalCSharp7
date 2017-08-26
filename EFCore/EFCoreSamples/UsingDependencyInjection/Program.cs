09354e835386ce07316f90d321c4a75d03a196a0
Christian Nagel <christian@christiannagel.com>
2017-08-26 18:06:05 +0200

    cleanup

--- EFCore/EFCoreSamples/UsingDependencyInjection/Program.cs
+++ EFCore/EFCoreSamples/UsingDependencyInjection/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Threading.Tasks;
 
 namespace UsingDependencyInjection

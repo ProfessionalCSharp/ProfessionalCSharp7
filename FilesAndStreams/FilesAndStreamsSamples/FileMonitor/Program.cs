5095196c68706ba468a4348a511b6d8c302193a0
Christian Nagel <christian@christiannagel.com>
2017-09-01 21:03:59 +0200

    file monitor

--- FilesAndStreams/FilesAndStreamsSamples/FileMonitor/Program.cs
+++ FilesAndStreams/FilesAndStreamsSamples/FileMonitor/Program.cs
@@ -5,27 +5,39 @@ namespace FileMonitor
 {
     public class Program
     {
+        private static FileSystemWatcher s_watcher;
+
         public static void Main(string[] args)
         {
-            WatchFiles("c:/test", "*.txt");
+            WatchFiles(".", "*.txt");
             Console.ReadLine();
+            UnWatchFiles();
         }
 
         public static void WatchFiles(string path, string filter)
         {
-            var watcher = new FileSystemWatcher(path, filter)
+            s_watcher = new FileSystemWatcher(path, filter)
             {
                 IncludeSubdirectories = true
             };
-            watcher.Created += OnFileChanged;
-            watcher.Changed += OnFileChanged;
-            watcher.Deleted += OnFileChanged;
-            watcher.Renamed += OnFileRenamed;
+            s_watcher.Created += OnFileChanged;
+            s_watcher.Changed += OnFileChanged;
+            s_watcher.Deleted += OnFileChanged;
+            s_watcher.Renamed += OnFileRenamed;
 
-            watcher.EnableRaisingEvents = true;
+            s_watcher.EnableRaisingEvents = true;
             Console.WriteLine("watching file changes...");
         }
 
+        public static void UnWatchFiles()
+        {
+            s_watcher.Created -= OnFileChanged;
+            s_watcher.Changed -= OnFileChanged;
+            s_watcher.Deleted -= OnFileChanged;
+            s_watcher.Renamed -= OnFileRenamed;
+            s_watcher.Dispose();
+            s_watcher = null;
+        }
 
         private static void OnFileRenamed(object sender, RenamedEventArgs e) =>
             Console.WriteLine($"file {e.OldName} {e.ChangeType} to {e.Name}");

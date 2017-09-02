2d1f6b94c925ce926769e28de221de99d2da8937
Christian Nagel <christian@christiannagel.com>
2017-09-02 10:23:44 +0200

    convention based sample

---
+++ Composition/ConventionBasedSample/ConventionBasedSample/CalculatorContract/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Wrox.ProCSharp.Composition
+{
+    public interface ICalculator
+    {
+        IList<IOperation> GetOperations();
+        double Operate(IOperation operation, double[] operands);
+    }
+}

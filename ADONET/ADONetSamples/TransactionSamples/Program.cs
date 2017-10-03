2b1629ad026e3fa39c238c9dd784adbf01b901e5
Christian Nagel <christian@christiannagel.com>
2017-10-03 17:51:33 +0200

    ado.net tx samples

--- ADONET/ADONetSamples/TransactionSamples/Program.cs
+++ ADONET/ADONetSamples/TransactionSamples/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace TransactionSamples
@@ -22,8 +23,8 @@ namespace TransactionSamples
 
                 try
                 {
-                    string sql = "INSERT INTO Sales.CreditCard (CardType, CardNumber, ExpMonth, ExpYear)" +
-                        "VALUES (@CardType, @CardNumber, @ExpMonth, @ExpYear); " +
+                    string sql = "INSERT INTO [ProCSharp].[Books] ([Title], [Publisher], [Isbn], [ReleaseDate]) " +
+                        "VALUES (@Title, @Publisher, @Isbn, @ReleaseDate);" +
                         "SELECT SCOPE_IDENTITY()";
                     var command = new SqlCommand
                     {
@@ -32,28 +33,28 @@ namespace TransactionSamples
                         Transaction = tx
                     };
 
-                    var p1 = new SqlParameter("CardType", SqlDbType.NVarChar, 50);
-                    var p2 = new SqlParameter("CardNumber", SqlDbType.NVarChar, 25);
-                    var p3 = new SqlParameter("ExpMonth", SqlDbType.TinyInt);
-                    var p4 = new SqlParameter("ExpYear", SqlDbType.SmallInt);
+                    var p1 = new SqlParameter("Title", SqlDbType.NVarChar, 50);
+                    var p2 = new SqlParameter("Publisher", SqlDbType.NVarChar, 50);
+                    var p3 = new SqlParameter("Isbn", SqlDbType.NVarChar, 20);
+                    var p4 = new SqlParameter("ReleaseDate", SqlDbType.Date);
                     command.Parameters.AddRange(new SqlParameter[] { p1, p2, p3, p4 });
                 
-                    command.Parameters["CardType"].Value = "MegaWoosh";
-                    command.Parameters["CardNumber"].Value = "08154711128";
-                    command.Parameters["ExpMonth"].Value = 4;
-                    command.Parameters["ExpYear"].Value = 2019;
+                    command.Parameters["Title"].Value = "Professional C# 8 and .NET Core 3.0";
+                    command.Parameters["Publisher"].Value = "Wrox Press";
+                    command.Parameters["Isbn"].Value = "42-08154711";
+                    command.Parameters["ReleaseDate"].Value = new DateTime(2020, 9, 2);
 
                     object id = await command.ExecuteScalarAsync();
                     Console.WriteLine($"record added with id: {id}");
 
-                    command.Parameters["CardType"].Value = "NeverLimits";
-                    command.Parameters["CardNumber"].Value = "987654321011";
-                    command.Parameters["ExpMonth"].Value = 12;
-                    command.Parameters["ExpYear"].Value = 2025;
+                    command.Parameters["Title"].Value = "Professional C# 9 and .NET Core 4.0";
+                    command.Parameters["Publisher"].Value = "Wrox Press";
+                    command.Parameters["Isbn"].Value = "42-08154711";
+                    command.Parameters["ReleaseDate"].Value = new DateTime(2022, 11, 2);
 
                     id = await command.ExecuteScalarAsync();
                     Console.WriteLine($"record added with id: {id}");
-                    // throw new Exception("abort");
+                   // throw new Exception("abort");
 
                     tx.Commit();
                 }
@@ -67,7 +68,9 @@ namespace TransactionSamples
 
         public static string GetConnectionString()
         {
-            var configurationBuilder = new ConfigurationBuilder().AddJsonFile("config.json");
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json");
 
             IConfiguration config = configurationBuilder.Build();
             string connectionString = config["Data:DefaultConnection:ConnectionString"];

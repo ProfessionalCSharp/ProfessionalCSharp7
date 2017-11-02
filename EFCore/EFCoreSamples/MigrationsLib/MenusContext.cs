676355682fa4aab733d143efa7bc9737f49307d0
Christian Nagel <christian@christiannagel.com>
2017-11-02 23:09:14 +0100

    migrations

---
+++ EFCore/EFCoreSamples/MigrationsLib/MenusContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MigrationsLib
+{
+    public class MenusContext : DbContext
+    {
+        public MenusContext(DbContextOptions<MenusContext> options): 
+            base(options) { }
+
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuCard> MenuCards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("mc");
+
+            modelBuilder.ApplyConfiguration(new MenuCardConfiguration());
+            modelBuilder.ApplyConfiguration(new MenuConfiguration());
+        }
+    }
+}
---
+++ EFCore/EFCoreSamples/MigrationsLib/Migrations/20171102212604_AddAllergens.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MigrationsLib.Migrations
+{
+    public partial class AddAllergens : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Allergens",
+                schema: "mc",
+                table: "Menus",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Allergens",
+                schema: "mc",
+                table: "Menus");
+        }
+    }
+}

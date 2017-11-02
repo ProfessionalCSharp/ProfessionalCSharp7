19efb2a839fff8f2f0f898461376a07741e61e8d
Christian Nagel <christian@christiannagel.com>
2017-11-02 18:23:58 +0100

    transaction samples

---
+++ EFCore/EFCoreSamples/TransactionsSample/Menu.cs
@@ -0,0 +1,17 @@
+﻿namespace TransactionSamples
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string Text { get; set; }
+        
+        public decimal Price { get; set; }
+
+        public int MenuCardId { get; set; }
+
+        public MenuCard MenuCard { get; set; }
+
+        public override string ToString() => Text;
+
+    }
+}

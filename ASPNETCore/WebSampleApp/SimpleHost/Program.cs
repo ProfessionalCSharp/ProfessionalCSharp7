1db75c25165d3ded7402062962aa28967dc367a7
Christian Nagel <christian@christiannagel.com>
2017-11-04 17:12:36 +0100

    simple host

---
+++ ASPNETCore/WebSampleApp/SimpleHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace SimpleHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.Start(async context =>
+            {
+                await context.Response.WriteAsync("<h1>A Simple Host!</h1>");
+            }).WaitForShutdown();
+        }
+    }
+}

279676b2eb273ffb9dc3c031e72003ecc895a6dc
Christian Nagel <christian@christiannagel.com>
2017-11-13 12:59:34 +0100

    views

---
+++ MVC/MVCSampleApp/MVCSampleApp/Controllers/ViewsDemoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCSampleApp.Models;
+
+namespace MVCSampleApp.Controllers
+{
+    public class ViewsDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PassingData()
+        {
+            ViewBag.MyData = "Hello from the controller";
+            return View();
+        }
+
+        private IEnumerable<Menu> GetSampleData() =>
+            new List<Menu>
+            {
+                new Menu
+                {
+                    Id=1,
+                    Text="Schweinsbraten mit Knödel und Sauerkraut",
+                    Price=6.9,
+                    Category="Main"
+                },
+                new Menu
+                {
+                    Id=2,
+                    Text="Erdäpfelgulasch mit Tofu und Gebäck",
+                    Price=6.9,
+                    Category="Vegetarian"
+                },
+                new Menu
+                {
+                    Id=3,
+                    Text="Tiroler Bauerngröst'l mit Spiegelei und Krautsalat",
+                    Price=6.9,
+                    Category="Main"
+                }
+            };
+
+        public IActionResult PassingAModel() =>
+            View(GetSampleData());
+
+    }
+}

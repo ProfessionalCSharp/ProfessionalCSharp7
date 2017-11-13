5d5b758fb5d39561dbd0c6c0fdc19dbd6f0320eb
Christian Nagel <christian@christiannagel.com>
2017-11-13 23:55:33 +0100

    HTML helpers intro

---
+++ MVC/MVCSampleApp/MVCSampleApp/Controllers/HtmlHelpersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCSampleApp.Models;
+
+namespace MVCSampleApp.Controllers
+{
+    public class HtmlHelpersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleHelper() => View();
+
+        public IActionResult HelperWithMenu() => View(GetSampleMenu());
+        private Menu GetSampleMenu() =>
+          new Menu
+          {
+              Id = 1,
+              Text = "Schweinsbraten mit Knödel und Sauerkraut",
+              Price = 6.9,
+              Date = new DateTime(2017, 11, 14),
+              Category = "Main"
+          };
+    }
+}

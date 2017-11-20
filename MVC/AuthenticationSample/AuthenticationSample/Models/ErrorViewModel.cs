5199059139746a03d61586b34c494392d05fd3b0
Christian Nagel <christian@christiannagel.com>
2017-11-20 17:21:27 +0100

    authentication sample

---
+++ MVC/AuthenticationSample/AuthenticationSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AuthenticationSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

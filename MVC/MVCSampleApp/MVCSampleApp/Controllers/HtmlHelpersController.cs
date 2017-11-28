23f0819bed56eaf56f5eaccb8614e93cd8ba83e9
Christian Nagel <christian@christiannagel.com>
2017-11-28 14:03:14 +0100

    HTML helpers

--- MVC/MVCSampleApp/MVCSampleApp/Controllers/HtmlHelpersController.cs
+++ MVC/MVCSampleApp/MVCSampleApp/Controllers/HtmlHelpersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MVCSampleApp.Extensions;
 using MVCSampleApp.Models;
 
 namespace MVCSampleApp.Controllers
@@ -26,5 +27,23 @@ namespace MVCSampleApp.Controllers
               Date = new DateTime(2017, 11, 14),
               Category = "Main"
           };
+
+        public IActionResult HtmlAttributes() => View();
+
+        public IActionResult HelperList()
+        {
+            var cars = new Dictionary<int, string>();
+            cars.Add(1, "Red Bull Racing");
+            cars.Add(2, "McLaren");
+            cars.Add(3, "Mercedes");
+            cars.Add(4, "Ferrari");
+            return View(cars.ToSelectListItems(4));
+        }
+
+        public IActionResult StronglyTypedMenu() => View(GetSampleMenu());
+
+        public IActionResult EditorExtensions() => View(GetSampleMenu());
+
+        public IActionResult Display() => View(GetSampleMenu());
     }
 }
--- MVC/MVCSampleApp/MVCSampleApp/Models/Menu.cs
+++ MVC/MVCSampleApp/MVCSampleApp/Models/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace MVCSampleApp.Models
     public partial class Menu
     {
         public int Id { get; set; }
+        [Required, StringLength(50)]
         public string Text { get; set; }
+        [Display(Name = "Price"), DisplayFormat(DataFormatString = "{0:c}")]
         public double Price { get; set; }
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
         public string Category { get; set; }
     }

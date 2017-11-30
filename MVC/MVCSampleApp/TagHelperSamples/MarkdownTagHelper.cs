624880d69a64109f6c102de40976735b28547cae
Christian Nagel <christian@christiannagel.com>
2017-11-30 21:39:06 +0100

    add DI with tag helper

--- MVC/MVCSampleApp/TagHelperSamples/MarkdownTagHelper.cs
+++ MVC/MVCSampleApp/TagHelperSamples/MarkdownTagHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Markdig;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.IO;
@@ -10,6 +11,9 @@ namespace TagHelperSamples
     [HtmlTargetElement(Attributes = "markdownfile")]
     public class MarkdownTagHelper : TagHelper
     {
+        private readonly IHostingEnvironment _env;
+        public MarkdownTagHelper(IHostingEnvironment env) => _env = env;
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (context == null) throw new ArgumentNullException(nameof(context));
@@ -18,7 +22,7 @@ namespace TagHelperSamples
             string markdown = string.Empty;
             if (MarkdownFile != null)
             {
-                string filename = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot", MarkdownFile);
+                string filename = Path.Combine(_env.WebRootPath, MarkdownFile);
                 markdown = File.ReadAllText(filename);
             }
             else

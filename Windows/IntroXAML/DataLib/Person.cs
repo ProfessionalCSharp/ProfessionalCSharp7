5c3c5a15ed0b0172a99ce17d6049313b8cce50ee
Christian Nagel <christian@christiannagel.com>
2017-12-07 05:09:39 +0100

    XAML Intro

---
+++ Windows/IntroXAML/DataLib/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace DataLib
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string ToString() => $"{FirstName} {LastName}";
+    }
+}

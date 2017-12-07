e139e36d474e4ef719b80ad23e5506eab8c2aa50
Christian Nagel <christian@christiannagel.com>
2017-12-07 05:15:21 +0100

    attached properties

---
+++ Windows/IntroXAML/AttachedProperty/MyAttachedPropertyProvider.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+
+namespace AttachedProperty
+{
+    public sealed class MyAttachedPropertyProvider : DependencyObject
+    {
+        //public string MySample
+        //{
+        //    get { return (string)GetValue(MySampleProperty); }
+        //    set { SetValue(MySampleProperty, value); }
+        //}
+
+        public static readonly DependencyProperty MySampleProperty =
+            DependencyProperty.RegisterAttached(
+                "MySample",
+                typeof(string),
+                typeof(MyAttachedPropertyProvider),
+                new PropertyMetadata(string.Empty));
+
+        public static void SetMySample(UIElement element, string value) =>
+            element.SetValue(MySampleProperty, value);
+
+        public static string GetMySample(UIElement element) =>
+            (string)element.GetValue(MySampleProperty);
+    }
+}

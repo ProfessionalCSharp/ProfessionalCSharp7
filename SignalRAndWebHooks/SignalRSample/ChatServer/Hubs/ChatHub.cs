9e8b246f13f66269d6889f18f6d8aaf1948cc18e
Christian Nagel <christian@christiannagel.com>
2017-12-12 17:39:12 +0100

    initial signalr

---
+++ SignalRAndWebHooks/SignalRSample/ChatServer/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string name, string message)
+        {
+            await Clients.All.InvokeAsync("BroadcastMessage", name, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- SignalRAndWebHooks/SignalRSample/ChatServer/Startup.cs
+++ SignalRAndWebHooks/SignalRSample/ChatServer/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChatServer.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -15,6 +16,8 @@ namespace ChatServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +27,13 @@ namespace ChatServer
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("chat");
+            });
 
             app.Run(async (context) =>
             {

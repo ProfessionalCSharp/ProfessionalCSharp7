dbb4b7d6bce3aec252498dd62ea231a349061427
Christian Nagel <christian@christiannagel.com>
2017-12-13 21:57:26 +0100

    page navigation

---
+++ Windows/PageNavigation/PageNavigation/SecondPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PageNavigation
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecondPage : NavigationPage
+    {
+        public SecondPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Data { get; set; }
+
+        public void OnNavigateToThirdPage()
+        {
+            Frame.Navigate(typeof(ThirdPage), Data);
+        }
+    }
+}

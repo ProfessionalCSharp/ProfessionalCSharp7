b1024da2a13d7c2454b6d9fd09405ea0ced56177
Christian Nagel <christian@christiannagel.com>
2017-12-17 16:38:37 +0100

    styles and resources

---
+++ Styles/StylesSamples/StylesAndResources/FrameworkElementExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml;
+
+namespace StylesAndResources
+{
+    public static class FrameworkElementExtensions
+    {
+        public static object TryFindResource(this FrameworkElement e, string key)
+        {
+            if (e == null) throw new ArgumentNullException(nameof(e));
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            if (e.Resources.ContainsKey(key))
+            {
+                return e.Resources[key];
+            }
+            else if (e.Parent is FrameworkElement parent)
+            {
+                TryFindResource(parent, key);
+            }
+            return null;
+        }
+    }
+}
---
+++ Styles/StylesSamples/StylesAndResources/ResourceDemoPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace StylesAndResources
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ResourceDemoPage : Page
+    {
+        public ResourceDemoPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnApplyResources(object sender, RoutedEventArgs e)
+        {
+            Control ctrl = sender as Control;
+            ctrl.Background = ctrl.TryFindResource("MyGradientBrush") as Brush;
+
+        }
+    }
+}

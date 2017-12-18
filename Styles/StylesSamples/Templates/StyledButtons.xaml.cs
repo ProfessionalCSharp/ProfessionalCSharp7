d6652cc18b379b9a4f24ae4bd305672835c17a7d
Christian Nagel <christian@christiannagel.com>
2017-12-18 22:51:21 +0100

    templates

---
+++ Styles/StylesSamples/Templates/StyledButtons.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Templates
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class StyledButtons : Page
+    {
+        public StyledButtons()
+        {
+            this.InitializeComponent();
+
+            this.countryButton.Content = new Country
+            {
+                Name = "Austria",
+                ImagePath = "images/Austria.bmp"
+            };
+        }
+    }
+}

067e8518a323d5ea60a0a46365101c2649cf5e1c
Christian Nagel <christian@christiannagel.com>
2017-12-18 21:02:15 +0100

    is pattern instead of as operator

--- Styles/StylesSamples/StylesAndResources/ResourceDemoPage.xaml.cs
+++ Styles/StylesSamples/StylesAndResources/ResourceDemoPage.xaml.cs
@@ -1,25 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace StylesAndResources
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class ResourceDemoPage : Page
     {
         public ResourceDemoPage()
@@ -29,9 +13,10 @@ namespace StylesAndResources
 
         private void OnApplyResources(object sender, RoutedEventArgs e)
         {
-            Control ctrl = sender as Control;
-            ctrl.Background = ctrl.TryFindResource("MyGradientBrush") as Brush;
-
+            if (sender is Control ctrl)
+            {
+                ctrl.Background = ctrl.TryFindResource("MyGradientBrush") as Brush;
+            }
         }
     }
 }

0c4d34578e2a1b07f72f64f4299423395203d41f
Christian Nagel <christian@christiannagel.com>
2017-12-23 22:18:49 +0100

    logging

---
+++ Diagnostics/LoggingSample/LoggingSample/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace LoggingSample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            RegisterServices();
+            await RunSampleAsync();
+            Console.ReadLine();
+        }
+
+        static async Task RunSampleAsync()
+        {
+            var controller = AppServices.GetService<SampleController>();
+            await controller.NetworkRequestSample2Async("https://csharp.christiannagel.com");
+        }
+
+        static void RegisterServices()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(options =>
+            {
+                options.AddEventSourceLogger();
+                options.AddConsole();
+                options.AddDebug().AddFilter(level => level >= LogLevel.Information);       
+            });
+            services.AddScoped<SampleController>();
+            AppServices = services.BuildServiceProvider();
+        }
+         
+        public static IServiceProvider AppServices { get; private set; }
+    }
+}
---
+++ Diagnostics/LoggingSample/LoggingSample/SampleController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LoggingSample
+{
+    class SampleController
+    {
+        private ILogger<SampleController> _logger;
+        public SampleController(ILogger<SampleController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task NetworkRequestSampleAsync(string url)
+        {
+            try
+            {
+                _logger.LogInformation("NetworkRequestSample started", url);
+                var client = new HttpClient();
+
+                string result = await client.GetStringAsync(url);
+                _logger.LogInformation("NetworkRequestSample completed", result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"NetworkRequestSample {ex.Message}", ex.Message, ex.HResult);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task NetworkRequestSample2Async(string url)
+        {
+            using (_logger.BeginScope("NetworkRequestSample"))
+            {
+                try
+                {
+                    _logger.Log(LogLevel.Trace, new EventId(42), url, null, (state, ex) => $"message: {state}, {ex?.Message}");
+                    _logger.LogInformation("NetworkRequestSample started {0}", url);
+                    var client = new HttpClient();
+                    string result = await client.GetStringAsync(url);
+                    _logger.LogInformation("NetworkRequestSample completed {0} --", result);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"NetworkRequestSample {ex.Message}", ex.Message, ex.HResult);
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}

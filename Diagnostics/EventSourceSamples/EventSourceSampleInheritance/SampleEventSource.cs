918f3b94a54d54eda6e553c2c35ecd85e652e1e3
Christian Nagel <christian@christiannagel.com>
2017-12-23 12:16:44 +0100

    eventsource with inheritance

---
+++ Diagnostics/EventSourceSamples/EventSourceSampleInheritance/SampleEventSource.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace EventSourceSampleInheritance
+{
+    public class SampleEventSource : EventSource
+    {
+        private SampleEventSource()
+            : base("Wrox-SampleEventSource2") { }
+
+        public static SampleEventSource Log = new SampleEventSource();
+        public void Startup() => WriteEvent(1);
+        
+        public void CallService(string url) => WriteEvent(2, url);
+        
+        public void CalledService(string url, int length) => WriteEvent(3, url, length);
+        
+        public void ServiceError(string message, int error) => WriteEvent(4, message, error);
+    }
+
+}

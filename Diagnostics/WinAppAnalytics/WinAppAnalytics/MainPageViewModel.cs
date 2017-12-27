593c90eae9cb521b9dc0d80149ee9c35821a571b
Christian Nagel <christian@christiannagel.com>
2017-12-27 17:07:00 +0100

    windows app analytics

---
+++ Diagnostics/WinAppAnalytics/WinAppAnalytics/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinAppAnalytics
+{
+    public class MainPageViewModel
+    {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+    }
+}

44f479cd7d1d9b3942daabdb02d28dc9ccc2b7d6
Christian Nagel <christian@christiannagel.com>
2017-12-28 14:48:36 +0100

    mocking samples

---
+++ Tests/MockingSamples/BooksLib/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using Framework;
+
+namespace BooksLib.Models
+{
+    public class Book : BindableObject
+    {
+        public int BookId { get; set; }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _publisher;
+        public string Publisher
+        {
+            get => _publisher;
+            set => SetProperty(ref _publisher, value);
+        }
+
+        public override string ToString() => Title;
+    }
+}
---
+++ Tests/MockingSamples/BooksLib/Repositories/IBooksRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BooksLib.Models;
+
+namespace BooksLib.Repositories
+{
+    public interface IBooksRepository
+    {
+        Task<Book> AddAsync(Book item);
+        Task<bool> DeleteAsync(int id);
+        Task<Book> GetItemAsync(int id);
+        Task<IEnumerable<Book>> GetItemsAsync();
+        Task<Book> UpdateAsync(Book item);
+    }
+}

5929f3eea7ad62fe13fac64b5578a56f5d913645
Christian Nagel <christian@christiannagel.com>
2018-01-03 15:44:24 +0100

    cleanup

--- Windows/ParallaxViewSample/ParallaxViewSample/MainPage.xaml.cs
+++ Windows/ParallaxViewSample/ParallaxViewSample/MainPage.xaml.cs
@@ -1,26 +1,9 @@
 ﻿using ParallaxViewSample.Models;
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ParallaxViewSample
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private IEnumerable<LunchMenu> _menusList = new List<LunchMenu>()
@@ -40,8 +23,6 @@ namespace ParallaxViewSample
 
         public MainPage()
         {
-         //   _menusList = _menusList.Concat(_menusList).Concat(_menusList).Concat(_menusList);
-
             this.InitializeComponent();
         }
     }

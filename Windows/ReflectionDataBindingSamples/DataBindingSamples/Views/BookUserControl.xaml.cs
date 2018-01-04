5c55748ec0612d12be522731db0892d5f0344912
Christian Nagel <christian@christiannagel.com>
2018-01-04 16:20:15 +0100

    old data binding

---
+++ Windows/ReflectionDataBindingSamples/DataBindingSamples/Views/BookUserControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using DataBindingSamples.Models;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DataBindingSamples.Views
+{
+    public sealed partial class BookUserControl : UserControl
+    {
+        public BookUserControl()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public Book Book
+        {
+            get { return (Book)GetValue(BookProperty); }
+            set { SetValue(BookProperty, value); }
+        }
+
+        public static readonly DependencyProperty BookProperty =
+            DependencyProperty.Register("Book", typeof(Book), typeof(BookUserControl), new PropertyMetadata(null));
+
+    }
+}

7ce9bcb9974477e559f12ca1b9056f3b8d570e6c
Christian Nagel <christian@christiannagel.com>
2018-01-04 16:37:42 +0100

    moved navigationview

--- Windows/NavigationViewSample/NavigationViewSample/App.xaml.cs
+++ Windows/NavigationViewSample/NavigationViewSample/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace NavigationView
+namespace NavigationViewSample
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
---
+++ Windows/NavigationViewSample/NavigationViewSample/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace NavigationViewSample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

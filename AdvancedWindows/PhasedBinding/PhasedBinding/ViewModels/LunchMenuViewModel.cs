cdbbde296c098ea298eb266762429114c5e31012
Christian Nagel <christian@christiannagel.com>
2018-01-04 21:09:30 +0100

    temp adv win

---
+++ AdvancedWindows/PhasedBinding/PhasedBinding/ViewModels/LunchMenuViewModel.cs
@@ -0,0 +1,37 @@
+﻿using PhasedBinding.Models;
+using PhasedBinding.Services;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PhasedBinding.ViewModels
+{
+    public class LunchMenuViewModel : INotifyPropertyChanged
+    {
+        private LunchMenuService _service = new LunchMenuService();
+
+        public LunchMenuViewModel()
+        {
+
+        }
+
+        public string IntroText { get; } = "A Lunch";
+
+        private LunchMenu _lunchMenu;
+        public LunchMenu LunchMenu
+        {
+            get => _lunchMenu;
+            set => Set(ref _lunchMenu, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Set<T>(ref T item, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(item, value))
+            {
+                item = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ AdvancedWindows/reflow/LayoutSamples/LayoutSamples/Views/VariableSizedWrapGridPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+namespace LayoutSamples.Views
+{
+    public sealed partial class VariableSizedWrapGridPage : Page
+    {
+        public VariableSizedWrapGridPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var r = new Random();
+            Grid[] items =
+                Enumerable.Range(0, 30).Select(i =>
+                {
+                    byte[] colorBytes = new byte[3];
+                    r.NextBytes(colorBytes);
+                    var rect = new Rectangle
+                    {
+                        Height = r.Next(40, 150),
+                        Width = r.Next(40, 150),
+                        Fill = new SolidColorBrush(new Color
+                        {
+                            R = colorBytes[0],
+                            G = colorBytes[1],
+                            B = colorBytes[2],
+                            A = 255
+                        })
+                    };
+                    var textBlock = new TextBlock
+                    {
+                        Text = (i + 1).ToString(),
+                        HorizontalAlignment =HorizontalAlignment.Center,
+                        VerticalAlignment = VerticalAlignment.Center
+                    };
+                    var grid = new Grid();
+                    grid.Children.Add(rect);
+                    grid.Children.Add(textBlock);
+                    return grid;
+                }).ToArray();
+
+            foreach (var item in items)
+            {
+                grid1.Children.Add(item);
+                Rectangle rect = item.Children.First() as Rectangle;
+                if (rect.Width > 50)
+                {
+                    int columnSpan = ((int)rect.Width / 50) + 1;
+                    VariableSizedWrapGrid.SetColumnSpan(item, columnSpan);
+                    int rowSpan = ((int)rect.Height / 50) + 1;
+                    VariableSizedWrapGrid.SetRowSpan(item, rowSpan);
+                }
+            }
+
+        }
+    }
+}

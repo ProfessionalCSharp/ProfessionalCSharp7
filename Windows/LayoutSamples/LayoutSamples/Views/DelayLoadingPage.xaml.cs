42bd1174af9f17252cfd96267a27e7bc5067d774
Christian Nagel <christian@christiannagel.com>
2018-01-04 20:46:56 +0100

    layout sample

---
+++ Windows/LayoutSamples/LayoutSamples/Views/DelayLoadingPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace LayoutSamples.Views
+{
+    public sealed partial class DelayLoadingPage : Page
+    {
+        public DelayLoadingPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnDeferLoad(object sender, RoutedEventArgs e)
+        {
+            FindName(nameof(deferGrid));
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+              Frame.CanGoBack ? AppViewBackButtonVisibility.Visible :
+              AppViewBackButtonVisibility.Collapsed;
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}

eff43c6c90f359badf8099d8020706fc9336478a
Christian Nagel <christian@christiannagel.com>
2018-01-06 07:12:38 +0100

    app lifetime

---
+++ AdvancedWindows/ApplicationLifetime/ApplicationLifetime/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using ApplicationLifetime.Utilities;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace ApplicationLifetime
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private BackButtonManager _backButtonManager;
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            _backButtonManager = new BackButtonManager(Frame);
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            _backButtonManager.Dispose();
+        }
+
+        public void GotoPage1()
+        {
+            ContentFrame.Navigate(typeof(Page1), Parameter1);
+
+        }
+
+        public string Parameter1 { get; set; }
+
+        public void GotoPage2()
+        {
+            ContentFrame.Navigate(typeof(Page2), Parameter2);
+        }
+
+        public string Parameter2 { get; set; }
+    }
+}
---
+++ AdvancedWindows/ApplicationLifetime/ApplicationLifetime/Page1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using ApplicationLifetime.Services;
+using ApplicationLifetime.Utilities;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace ApplicationLifetime
+{
+    public sealed partial class Page1 : Page
+    {
+        public Page1()
+        {
+            this.InitializeComponent();
+        }
+
+        private BackButtonManager _backButtonManager;
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            _backButtonManager = new BackButtonManager(Frame);
+
+
+            ReceivedContent = e.Parameter?.ToString() ?? string.Empty;
+            Bindings.Update();
+        }
+
+        public string ReceivedContent { get; private set; }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            _backButtonManager.Dispose();
+        }
+
+        public void GotoPage2()
+        {
+            Frame.Navigate(typeof(Page2), Parameter1);
+        }
+
+        public string Parameter1 { get; set; }
+
+        public DataManager Data => DataManager.Instance;
+    }
+}

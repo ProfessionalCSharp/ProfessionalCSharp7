e560403b8616d32fa067b4245e8402b639f91bf5
Christian Nagel <christian@christiannagel.com>
2018-01-06 09:30:40 +0100

    app services

---
+++ AdvancedWindows/AppServices/BooksCacheModel/BooksRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BooksCacheModel
+{
+    public class BooksRepository
+    {
+        private readonly List<Book> _books = new List<Book>()
+        {
+            new Book {Title = "Professional C# 7 and .NET Core 2", Publisher = "Wrox Press" }
+        };
+        public IEnumerable<Book> Books => _books;
+
+        private BooksRepository()
+        {
+        }
+
+        public static BooksRepository Instance = new BooksRepository();
+
+        public void AddBook(Book book) => _books.Add(book);
+    }
+}

82d77d8e3728029f41a030453489b17e68dadfee
Christian Nagel <christian@christiannagel.com>
2018-01-08 22:05:53 +0100

    change libs to shared folder

--- Xamarin/BooksSample/BooksLib/Services/IMessageService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace BooksLib.Services
-{
-    public interface IMessageService
-    {
-        Task ShowMessageAsync(string message);
-    }
-}
--- Xamarin/BooksSample/Framework/ViewModels/ItemViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Framework.ViewModels
-{
-    public abstract class ItemViewModel<T> : ViewModelBase, IItemViewModel<T>
-    {
-        public virtual T Item { get; set; }
-    }
-}

2c92d8de12f59d8063957ae9a55cf5fc00a3afc4
Christian Nagel <christian@christiannagel.com>
2018-01-10 08:29:18 +0100

    initial hello ios

---
+++ Xamarin/HelloiOS/HelloiOS/ViewController.designer.cs
@@ -0,0 +1,24 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace HelloiOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Action ("OnButtonClick:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void OnButtonClick (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}

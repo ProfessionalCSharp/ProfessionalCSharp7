b801f14d08b708fff2c983c19ba5a0d52c6745f3
Christian Nagel <christian@christiannagel.com>
2018-01-13 22:34:56 +0100

    signalr server and html client

--- SignalRAndWebHooks/SignalRSample/ChatServer/Hubs/ChatHub.cs
+++ SignalRAndWebHooks/SignalRSample/ChatServer/Hubs/ChatHub.cs
@@ -1,26 +1,12 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ChatServer.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string name, string message)
+        public void Send(string name, string message)
         {
-            await Clients.All.InvokeAsync("BroadcastMessage", name, message);
-        }
-
-        public override Task OnConnectedAsync()
-        {
-            return base.OnConnectedAsync();
-        }
-
-        public override Task OnDisconnectedAsync(Exception exception)
-        {
-            return base.OnDisconnectedAsync(exception);
+            Clients.All.InvokeAsync("BroadcastMessage", name, message);
         }
     }
 }
--- SignalRAndWebHooks/SignalRSample/ChatServer/Startup.cs
+++ SignalRAndWebHooks/SignalRSample/ChatServer/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ChatServer.Hubs;
+﻿using ChatServer.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,15 +8,11 @@ namespace ChatServer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())

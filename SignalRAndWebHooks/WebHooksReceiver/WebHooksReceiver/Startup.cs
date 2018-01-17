2d131a3de285ef3af1ca761242d7877cd63d3f9f
Christian Nagel <christian@christiannagel.com>
2018-01-17 20:43:00 +0100

    webhooks

---
+++ SignalRAndWebHooks/WebHooksReceiver/WebHooksReceiver/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebHooksReceiver.Services;
+
+namespace WebHooksReceiver
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddDropboxWebHooks()
+                .AddGitHubWebHooks();
+
+            services.AddSingleton<IStorageQueueService, StorageQueueService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}

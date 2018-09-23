26e2c68d8bd80635b198dae50b430d03f2a2b505
Christian Nagel <christian@christiannagel.com>
2018-09-23 15:25:46 +0200

    package updates

--- SignalRAndWebHooks/SignalRSample/ChatServer/Startup.cs
+++ SignalRAndWebHooks/SignalRSample/ChatServer/Startup.cs
@@ -11,7 +11,7 @@ namespace ChatServer
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
+            services.AddSignalR().AddAzureSignalR();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -23,7 +23,12 @@ namespace ChatServer
 
             app.UseFileServer();
 
-            app.UseSignalR(routes =>
+            //app.UseSignalR(routes =>
+            //{
+            //    routes.MapHub<ChatHub>("/chat");
+            //    routes.MapHub<GroupChatHub>("/groupchat");
+            //});
+            app.UseAzureSignalR(routes =>
             {
                 routes.MapHub<ChatHub>("/chat");
                 routes.MapHub<GroupChatHub>("/groupchat");

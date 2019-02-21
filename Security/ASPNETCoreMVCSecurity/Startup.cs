cf083cbb389644c40a0f3ad1dd6d1036c80b80f0
Christian Nagel <christian@christiannagel.com>
2019-02-21 21:44:00 +0100

    package updates for 2.1 lts

--- Security/ASPNETCoreMVCSecurity/Startup.cs
+++ Security/ASPNETCoreMVCSecurity/Startup.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Http;
 using System.Text.Encodings.Web;
 
 namespace ASPNETCoreMVCSecurity

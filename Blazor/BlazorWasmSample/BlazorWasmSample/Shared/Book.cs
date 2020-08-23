9555839c5357b00d22057b427ed3ee8e6e2d35ac
Christian Nagel <christian@christiannagel.com>
2020-08-23 14:26:52 +0200

    initial blazor wasm

---
+++ Blazor/BlazorWasmSample/BlazorWasmSample/Shared/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorWasmSample.Shared
+{
+    poublic class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+    }
+}

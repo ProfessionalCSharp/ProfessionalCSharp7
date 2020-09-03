152ac2fd7975afabe2e1eede3e1f302ec7c1c892
Christian Nagel <christian@christiannagel.com>
2020-09-03 11:56:41 +0200

    init only props

---
+++ CSharp9/CSharp9Samples/InitOnlyProps/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace InitOnlyProps
+{
+    public class Book
+    {
+        public string Title { get; init; } = string.Empty;
+        public string? Publisher { get; init; }
+
+        private readonly string? _isbn;
+
+        public string? Isbn
+        {
+            get => _isbn;
+            init => _isbn = value;
+        }
+    }
+}
---
+++ CSharp9/CSharp9Samples/InitOnlyProps/Program.cs
@@ -0,0 +1,5 @@
+﻿using InitOnlyProps;
+using System;
+
+var book = new Book { Title = "Professional C# 9", Publisher = "Wrox Press" };
+Console.WriteLine(book.Title);

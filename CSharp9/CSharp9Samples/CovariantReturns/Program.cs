a7089f98a13cbcd03f3c10369da6f907c54df7b4
Christian Nagel <christian@christiannagel.com>
2020-09-05 14:41:00 +0200

    covariant returns

---
+++ CSharp9/CSharp9Samples/CovariantReturns/Program.cs
@@ -0,0 +1,12 @@
+﻿using CovariantReturns;
+using System;
+
+Shape shape = new Ellipse(20, 20, 100, 50);
+Shape shape2 = shape.Clone();
+// Ellipse e1 = shape.Clone(); not possible
+if (shape2 is Ellipse e)
+{
+    Console.WriteLine($"it's a ellipse {e}");
+}
+Rectangle rectangle = new(50, 50, 80, 70);
+Rectangle rectangle12 = rectangle.Clone();
---
+++ CSharp9/CSharp9Samples/CovariantReturns/Shapes.cs
@@ -0,0 +1,52 @@
+﻿namespace CovariantReturns
+{
+
+    public abstract class Shape
+    {
+        public Shape(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+        public Shape(Shape original)
+        {
+            X = original.X;
+            Y = original.Y;
+            Width = original.Width;
+            Height = original.Height;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+        public int Width { get; }
+        public int Height { get; }
+        public abstract Shape Clone();
+    }
+
+    public class Ellipse : Shape
+     {
+        public Ellipse(int x, int y, int width, int height): base(x, y, width, height)
+        {
+        }
+        public Ellipse(Ellipse original) : base(original)
+        {
+        }
+
+        public override Ellipse Clone() => new Ellipse(this);
+    }
+
+    public class Rectangle : Shape
+    {
+        public Rectangle(int x, int y, int width, int height) : base(x, y, width, height)
+        { }
+    
+        public Rectangle(Rectangle original) : base(original)
+        {
+        }
+
+        public override Rectangle Clone() => new Rectangle(this);
+    }
+
+}

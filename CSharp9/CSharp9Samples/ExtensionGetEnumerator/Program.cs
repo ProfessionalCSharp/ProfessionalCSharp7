95beb47c6389124e7a150146623f6d13aff3b72f
Christian Nagel <christian@christiannagel.com>
2020-09-06 11:22:45 +0200

    extension GetEnumerator

---
+++ CSharp9/CSharp9Samples/ExtensionGetEnumerator/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using ExtensionGetEnumerator;
+
+foreach (var i in 3..8)
+{
+    Console.WriteLine(i);
+}
---
+++ CSharp9/CSharp9Samples/ExtensionGetEnumerator/RangeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionGetEnumerator
+{
+    public static class RangeExtensions
+    {
+        public static IEnumerator<int> GetEnumerator(this Range range)
+        {
+            if (range.Start.IsFromEnd || range.End.IsFromEnd) throw new InvalidOperationException("hat operator not allowed");
+
+            for (int i = range.Start.Value; i < range.End.Value; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
